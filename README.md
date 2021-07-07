df6104ad321a5750a78016f79a0aefb42e515d85
Ross Vernal <ross_vernal@hotmail.com>
2021-07-07 16:11:13 +0100

    Updates to the script

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 A Repo to get my BME280 to send messages to Dweet.io
 
 Contact me for more info!
+
+Prerequisites:
+
+sudo pip install pimoroni-bme280 smbus
+sudo pip install dweepy
--- all-values.py
+++ all-values.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 import time
+import dweepy
 from smbus import SMBus
 from bme280 import BME280
+from datetime import datetime, timedelta
 
 print("""all-values.py - Read temperature, pressure, and humidity
 Press Ctrl+C to exit!
@@ -14,7 +16,16 @@ bme280 = BME280(i2c_dev=bus)
 
 while True:
     temperature = bme280.get_temperature()
+    temperaturePrint = "{:05.2f}".format(temperature)
     pressure = bme280.get_pressure()
     humidity = bme280.get_humidity()
     print('{:05.2f}*C {:05.2f}hPa {:05.2f}%'.format(temperature, pressure, humidity))
-    time.sleep(1)
+
+    dweepy.dweet_for('ingeniuus_ross_room', {'temperature': temperaturePrint })
+    {
+        u'content': {u'temperature': temperaturePrint},
+        u'created': datetime.now() + timedelta(hours=1),
+        u'thing': u'ingeniuus_ross_room'
+    }
+
+    time.sleep(10)
