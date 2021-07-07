e05111b79f854ba687cb770cf0fe36a94c48d7e0
Ross Vernal <ross_vernal@hotmail.com>
2021-07-07 15:25:06 +0100

    Create a script to get all the values from the BME280

---
+++ all-values.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import time
+from smbus import SMBus
+from bme280 import BME280
+
+print("""all-values.py - Read temperature, pressure, and humidity
+Press Ctrl+C to exit!
+""")
+
+# Initialise the BME280
+bus = SMBus(1)
+bme280 = BME280(i2c_dev=bus)
+
+while True:
+    temperature = bme280.get_temperature()
+    pressure = bme280.get_pressure()
+    humidity = bme280.get_humidity()
+    print('{:05.2f}*C {:05.2f}hPa {:05.2f}%'.format(temperature, pressure, humidity))
+    time.sleep(1)
