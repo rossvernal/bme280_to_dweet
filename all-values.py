2df111c1562121f77f42dc87ebade7a0523726e7
Ross Vernal <ross_vernal@hotmail.com>
2021-11-06 18:57:22 +0000

    Update to 5 mins and include pressure and humidity

--- all-values.py
+++ all-values.py
@@ -16,16 +16,19 @@ bme280 = BME280(i2c_dev=bus)
 
 while True:
     temperature = bme280.get_temperature()
-    temperaturePrint = "{:05.2f}".format(temperature)
+    temperaturePrint = "{:05.2f}*C".format(temperature)
     pressure = bme280.get_pressure()
+    pressurePrint =  "{:05.2f}hPa".format(pressure)
     humidity = bme280.get_humidity()
+    humidityPrint = "{:05.2f}%".format(humidity)
     print('{:05.2f}*C {:05.2f}hPa {:05.2f}%'.format(temperature, pressure, humidity))
 
-    dweepy.dweet_for('ingeniuus_ross_room', {'temperature': temperaturePrint })
+    dweepy.dweet_for('ingeniuus_ross_room', {'temperature': temperaturePrint, 'pressure': pressurePrint, 'humidity': humidityPrint })
     {
-        u'content': {u'temperature': temperaturePrint},
+        u'content': {u'temperature': temperaturePrint, u'pressure': pressurePrint, u'humidity': humidityPrint},
         u'created': datetime.now() + timedelta(hours=1),
         u'thing': u'ingeniuus_ross_room'
     }
 
-    time.sleep(10)
+    #Update every 5 mins
+    time.sleep(300)
