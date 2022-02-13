f976461b13752adda409672281ae162855ba2858
Ross Vernal <ross_vernal@hotmail.com>
2022-02-13 19:00:59 +0000

    Update script to include degree symbol

--- all-values.py
+++ all-values.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import time
 import dweepy
 from smbus import SMBus
@@ -14,16 +13,19 @@ Press Ctrl+C to exit!
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
 
+# Initialise other strings 
+degree_sign = u"\N{DEGREE SIGN}"
+
 while True:
     temperature = bme280.get_temperature()
-    temperaturePrint = "{:05.2f}°C".format(temperature)
+    temperaturePrint = "{:05.2f}".format(temperature) + degree_sign + "C"
     pressure = bme280.get_pressure()
     pressurePrint =  "{:05.2f}mb".format(pressure)
     humidity = bme280.get_humidity()
     humidityPrint = "{:05.2f}%".format(humidity)
-    print('{:05.2f}°C {:05.2f}mb {:05.2f}%'.format(temperature, pressure, humidity))
+    print(temperaturePrint + " " + pressurePrint + " " + humidityPrint)
 
-    dweepy.dweet_for('ingeniuus_ross_room', {'temperature': temperaturePrint, 'pressure': pressurePrint, 'humidity': humidityPrint })
+    dweepy.dweet_for('rvs_room', {'temperature': temperaturePrint, 'pressure': pressurePrint, 'humidity': humidityPrint })
     {
         u'content': {u'temperature': temperaturePrint, u'pressure': pressurePrint, u'humidity': humidityPrint},
         u'created': datetime.now() + timedelta(hours=1),
